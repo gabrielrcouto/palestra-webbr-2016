131cd5403a63c3936bfbe3f65fffe56519935790
Gabriel Rodrigues Couto <gabrielrcouto@gmail.com>
2016-10-14 09:37:48 -0300

    Correção do host para websockets

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -27,11 +27,7 @@ $app->get('/', function () use ($app) {
         ]);
     }
 
-    //Caso seja rodado no localhost:8000, por exemplo
-    //retira os : do nome do servidor
     $host = $_SERVER['HTTP_HOST'];
-    $hostExplode = explode(':', $host);
-    $host = $hostExplode[0];
 
     $data = [
         'host' => $_SERVER['HTTP_HOST'],
